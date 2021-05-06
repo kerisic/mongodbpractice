5a21b6d4e2464e80ff38974f73c66109a6236bb4
Christine Chee <christinechee88@hotmail.com>
2021-05-06 20:56:06 +0100

    fix mongo

--- myApp.js
+++ myApp.js
@@ -1,10 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-let Person;
-
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
 const personSchema = new Schema({
   name: { type: String, required: true },
