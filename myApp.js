08a7b633939048620b1b0be78fc426081bb6d249
Christine Chee <christinechee88@hotmail.com>
2021-05-07 20:43:15 +0100

    Find one

--- myApp.js
+++ myApp.js
@@ -41,7 +41,10 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({favouriteFoods: food}, function (err, data) {
+    if (err) return console.log(err);
+    done(null, data)
+  })
 };
 
 const findPersonById = (personId, done) => {
