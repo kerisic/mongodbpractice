a3844df34439cf75f59163a1fb327666a5f7e191
Christine Chee <christinechee88@hotmail.com>
2021-05-10 14:59:26 +0100

    querychain

--- myApp.js
+++ myApp.js
@@ -99,8 +99,14 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch})
+  .sort({name: 1})
+  .limit(2)
+  .select({age: 0})
+  .exec((err, data) => {
+    if (err) return console.log(err);
+    done(null, data);
+  })
 };
 
 /** **Well Done !!**
